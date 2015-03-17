1f2c3c2537ffab8b95543d01e8e1c0ff1a60bbed
Matthew Avant <mavant@palantir.com>
2015-03-16 17:18:52 -0700

    Clean up personal manifest

--- modules/people/manifests/mavant.pp
+++ modules/people/manifests/mavant.pp
@@ -1,27 +1,4 @@
 class people::mavant {
-  include karabiner
-  include karabiner::login_item
-  include java
-  include iterm2::stable
-  include vagrant
-  include onepassword
-  include skype
-  include evernote
-  include dropbox
-  include firefox
-  include chrome
-  include hipchat
-  include brewcask
-  include osx::global::enable_standard_function_keys
-  include osx::dock::autohide
-  include osx::finder::show_hidden_files
-  include osx::finder::unhide_library
-  include osx::finder::show_all_filename_extensions
-  include osx::no_network_dsstores
-  include osx::software_update
-  include osx::keyboard::capslock_to_control
-  include zsh
-
   $home     = "/Users/${::boxen_user}"
   $srcdir   = "/Volumes/git"
   $src      = "/src"
@@ -35,17 +12,64 @@ class people::mavant {
       target => $srcdir
   }
 
+  include java
+  include iterm2::stable
+  include vagrant
+  include onepassword
+  include skype
+  include evernote
+  include dropbox
+  include firefox
+  include chrome
+  include hipchat
+  include brewcask
+  include zsh
+
+  # Set various OSX options
+  include osx::global::enable_standard_function_keys
+  include osx::dock::autohide
+  include osx::finder::show_hidden_files
+  include osx::finder::unhide_library
+  include osx::finder::show_all_filename_extensions
+  include osx::no_network_dsstores
+  include osx::software_update
+  include osx::keyboard::capslock_to_control
+
   # Because some asshole decided the git module should manage my gitignore for me.
   Git::Config::Global <| title == 'core.excludesfile' |> {
       value => "${home}/.gitignore_global"
   }
 
+  # Karabiner config
+  include karabiner
+  include karabiner::login_item
   karabiner::remap{ 'controlL2controlL_escape': }
   karabiner::set{ 'repeat.initial_wait':
       value => '200'
   }
   karabiner::set{ 'repeat.wait':
       value => '33'
+  }
+
+  # Homebrew packages
+  package {
+      [
+          'findutils',
+          'gh',
+          'gnu-tar',
+          'coreutils',
+          'curl',
+          'fzf',
+          'lua',
+          'luajit',
+          'task',
+          'the_platinum_searcher',
+          'the_silver_searcher',
+          'tree',
+          'tor',
+          'tmux',
+          'vifm'
+      ]:
   }
 
   package { 'vim':
